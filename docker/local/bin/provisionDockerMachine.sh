28c42be86c6d9a2ad5dbf6fbc2829e00a8e78a94
Simon Rönnberg <simon.ronnberg@codemill.se>
2015-09-15 10:03:09 +0200

    Refactoring provision script

--- docker/local/bin/provisionDockerMachine.sh
+++ docker/local/bin/provisionDockerMachine.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+MACHINE_PATH=${DOCKER_MACHINE_PATH:-$HOME/machine}
 NAME=$1
 
 docker-machine ssh $NAME -- mkdir -p /opt/utils/
-docker-machine scp -r $HOME/machine/bin $NAME:/opt/utils/
-docker-machine scp -r $HOME/machine/profile.d $NAME:/opt/utils/
+docker-machine scp -r $MACHINE_PATH/bin $NAME:/opt/utils/
+docker-machine scp -r $MACHINE_PATH/profile.d $NAME:/opt/utils/
 docker-machine ssh $NAME -- sudo ln -s /opt/utils/profile.d/docker-env.sh /etc/profile.d/
