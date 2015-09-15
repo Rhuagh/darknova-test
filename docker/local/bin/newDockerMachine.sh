55da19fa7e766cae4c29970836a8a5866a2d2c0e
Simon Rönnberg <simon.ronnberg@codemill.se>
2015-09-15 09:59:24 +0200

    Refactoring docker scripts

--- docker/local/bin/newDockerMachine.sh
+++ docker/local/bin/newDockerMachine.sh
@@ -5,6 +5,9 @@ NAME=$1
 docker-machine create --driver virtualbox $NAME
 
 if [[ ! -f $HOME/.docker-$NAME.rc ]]; then
-    newDockerMachineScript.sh $NAME
+    echo "eval \"\$(docker-machine env $NAME)\"" > ~/.docker-$NAME.rc
+    echo "export DOCKER_HOST_IP=\$(docker-machine ip $NAME)" >> ~/.docker-$NAME.rc
+    echo "source \$HOME/.docker-ce-base.rc" >> ~/.docker-$NAME.rc
 fi
+
 provisionDockerMachine.sh $NAME
--- docker/local/bin/newDockerMachineScript.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-NAME=$1
-
-echo "eval \"\$(docker-machine env $NAME)\"" > ~/.docker-$NAME.rc
-echo "export DOCKER_HOST_IP=\$(docker-machine ip $NAME)" >> ~/.docker-$NAME.rc
-echo "source \$HOME/.docker-ce-base.rc" >> ~/.docker-$NAME.rc
