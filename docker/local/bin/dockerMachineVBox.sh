9c900b739c1f52c209b456dba9c98318d1cda62c
Simon Rönnberg <simon.ronnberg@codemill.se>
2015-09-15 10:51:35 +0200

    Fixed issues in docker machine script

--- docker/local/bin/dockerMachineVBox.sh
+++ docker/local/bin/dockerMachineVBox.sh
@@ -1,10 +1,25 @@
 #!/bin/sh
 
-COMMAND=shift
-NAME=shift
+COMMAND=$1
+shift
+NAME=$1
+shift
 SCRIPT_PATH=/var/lib/docker/opt/utils/
 
 [[ -f $HOME/.docker-$NAME.rc ]] && source $HOME/.docker-$NAME.rc
+
+function provisionMachine() {
+    MACHINE_PATH=${DOCKER_MACHINE_PATH:-$HOME/machine}
+    docker-machine ssh $NAME -- mkdir -p /opt/utils/
+    docker-machine ssh $NAME -- sudo mkdir -p $SCRIPT_PATH
+    docker-machine scp -r $MACHINE_PATH/bin $NAME:/opt/utils/
+    docker-machine scp -r $MACHINE_PATH/profile.d $NAME:/opt/utils/
+    docker-machine ssh $NAME -- sudo cp -r /opt/utils/* $SCRIPT_PATH
+}
+
+function afterStart() {
+    docker-machine ssh $1 -- sudo ln -s $SCRIPT_PATH/profile.d/docker-env.sh /etc/profile.d/
+}
 
 case "$COMMAND" in
     create)
@@ -14,6 +29,7 @@ case "$COMMAND" in
             echo "export DOCKER_HOST_IP=\$(docker-machine ip $NAME)" >> ~/.docker-$NAME.rc
             echo "source \$HOME/.docker-ce-base.rc" >> ~/.docker-$NAME.rc
         fi
+        source $HOME/.docker-$NAME.rc
         provisionMachine $NAME
         afterStart $NAME
         ;;
@@ -22,6 +38,7 @@ case "$COMMAND" in
         ;;
     start)
         docker-machine start $NAME
+        source $HOME/.docker-$NAME.rc
         afterStart $NAME
         ;;
     stop)
@@ -42,17 +59,5 @@ case "$COMMAND" in
     *)
         echo "Unknown command: $COMMAND"
         ;;
-done
+esac
 
-function provisionMachine() {
-    MACHINE_PATH=${DOCKER_MACHINE_PATH:-$HOME/machine}
-    docker-machine ssh $NAME -- mkdir -p /opt/utils/
-    docker-machine ssh $NAME -- sudo mkdir -p $SCRIPT_PATH
-    docker-machine scp -r $MACHINE_PATH/bin $NAME:/opt/utils/
-    docker-machine scp -r $MACHINE_PATH/profile.d $NAME:/opt/utils/
-    docker-machine ssh $NAME -- sudo cp -r /opt/utils/* $SCRIPT_PATH
-}
-
-function afterStart() {
-    docker-machine ssh $1 -- sudo ln -s $SCRIPT_PATH/profile.d/docker-env.sh /etc/profile.d/
-}
